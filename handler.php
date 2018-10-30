b9796f1bc1bf2d878660b5246d21e07fb6b6e788
Kristina <42621125+Kristina091002@users.noreply.github.com>
2018-10-30 14:10:51 -0600

    Create handler.php

---
+++ handler.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = $_GET['name'];
+$email = $_GET['email'];
+$password = $_GET['password'];
+
+echo "Welcome ".$name;
+echo "<br/>";
+echo "Please verify that your e-mail address is: ".$email;
+
+?>
